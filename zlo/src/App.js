3a64eeddb861293a007cc4c3ad50520cf651d5e6
Astemir <derbe.astemir@mail.ru>
2024-04-09 15:38:55 +0300

    добавил счетчик траффика

--- zlo/src/App.js
+++ zlo/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import MainPage from './components/MainPage/MainPage';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <div>
       <Navbar />
       <MainPage />
+      <SpeedInsights/>
     </div>
   );
 }
