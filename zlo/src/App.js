6ec00547602e04a6de2d27b58e44d53416d02709
Astemir <derbe.astemir@mail.ru>
2024-04-09 15:46:10 +0300

    аналитика

--- zlo/src/App.js
+++ zlo/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import MainPage from './components/MainPage/MainPage';
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <MainPage />
       <SpeedInsights/>
+      <Analytics/>
     </div>
   );
 }
